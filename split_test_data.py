2e73d60b179e461be3cd3c7dc81be12bd3d718c7
jdchen623 <jchen623@stanford.edu>
2019-02-27 00:32:31 -0800

    split into test and val

--- split_test_data.py
+++ split_test_data.py
@@ -5,18 +5,19 @@ import shutil
 TEST_DIR = "data/test"
 VAL_DIR = "data/val"
 
-test_files = [name for name in os.listdir(TEST_DIR) if os.path.isfile(name)]
-val_files = [name for name in os.listdir(VAL_DIR) if os.path.isfile(name)]
-print("number of files currently in test directory:". len(test_files))
-print("number of files currently in val directory:". len(val_files))
+test_files = [name for name in os.listdir(TEST_DIR) if name.endswith("jpg")]
+val_files = [name for name in os.listdir(VAL_DIR) if name.endswith("jpg")]
 
-for file_index in len(test_files)/2:
+print("number of files currently in test directory:", len(test_files))
+print("number of files currently in val directory:", len(val_files))
+
+for file_index in range(0, int(len(test_files)/2)):
     file_name = test_files[file_index]
-    file_path = os.join("test/", file_name)
-    new_path = os.join("val/", file_name)
-    os.rename(file__path, new_path)
+    file_path = os.path.join("data/test/", file_name)
+    new_path = os.path.join("data/val/", file_name)
+    os.rename(file_path, new_path)
 
-test_files = [name for name in os.listdir(TEST_DIR) if os.path.isfile(name)]
-val_files = [name for name in os.listdir(VAL_DIR) if os.path.isfile(name)]
-print("number of files currently in test directory after split:". len(test_files))
-print("number of files currently in val directory after split:". len(val_files))
+test_files = [name for name in os.listdir(TEST_DIR) if name.endswith("jpg")]
+val_files = [name for name in os.listdir(VAL_DIR) if name.endswith("jpg")]
+print("number of files currently in test directory after split:", len(test_files))
+print("number of files currently in val directory after split:", len(val_files))
