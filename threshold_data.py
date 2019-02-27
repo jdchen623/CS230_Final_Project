9d6c0bbb24f6ee8088abdd8ec1febcf329265a0b
jdchen623 <jchen623@stanford.edu>
2019-02-27 01:08:39 -0800

    created text files

--- threshold_data.py
+++ threshold_data.py
@@ -4,8 +4,10 @@ import pandas
 from collections import OrderedDict
 
 NUM_IMAGES_THRESHOLD = 500
+DIR_PATH = "data/train/"
+LABELS_FILE = "train_labels.txt"
 
-def makeFileNameAndStylePairs():
+def makeFileNameAndStylePairsCsv():
 
     #use pandas to load CSV data into filenames and styles
     colnames = ["new_filename", "style_type"]
@@ -37,7 +39,7 @@ def makeFileNameAndStylePairs():
             style_dict[value] = 1
 
     dd = OrderedDict(sorted(style_dict.items(), key=lambda x: -x[1]))
-    
+
     return dict, style_dict
 
 def meets_threshold(style_dict, count_dict, filename):
@@ -49,24 +51,58 @@ def meets_threshold(style_dict, count_dict, filename):
             assert("Missing style")
     else:
         assert("Missing filename")
-    print(len(files_to_delete))
+
+def make_label_txt_file(style_dict, label_dict, data_path):
+    filepath = LABELS_FILE
+    with open(filepath, 'a') as label_file:
+        for filename in os.listdir(data_path):
+            if filename in style_dict:
+                file_style = style_dict[filename]
+                if file_style in label_dict:
+                    line = filename + " " + str(label_dict[file_style]) + "\n"
+                    label_file.write(line)
+                else:
+                    assert("Missing style")
+            else:
+                assert("Missing file")
+
+def remove_nonthreshold_files(style_dict, count_dict):
+    files_to_delete = []
+    for filename in os.listdir(DIR_PATH):
+        if filename.endswith(".jpg"):
+            if not meets_threshold(style_dict, count_dict, filename):
+                files_to_delete.append(filename)
+
+    for filename in files_to_delete:
+        filepath = DIR_PATH  + filename
+        os.remove(filepath)
+
+def get_label_dict(style_dict, data_path):
+    styles = []
+    for filename in os.listdir(data_path):
+        if filename in style_dict and style_dict[filename] not in styles and style_dict[filename] == style_dict[filename] :
+            styles.append(style_dict[filename])
+
+    styles = sorted(styles)
+    print("styles:",len(styles))
+    label_dict = {}
+    label = 0
+    for style in styles:
+        label_dict[style] = label
+        label += 1
+    return label_dict
+
+def remove_nan(style_dict, dir_path):
+    for filename in os.listdir(dir_path):
+        if filename in style_dict and style_dict[filename] and style_dict[filename] != style_dict[filename]:
+            file_to_delete = dir_path + filename
+            os.remove(file_to_delete)
 
 
-style_dict, count_dict = makeFileNameAndStylePairs()
 
-files_to_delete = []
-for filename in os.listdir("test"):
-    if filename.endswith(".jpg"):
-        if not meets_threshold(style_dict, count_dict, filename):
-            files_to_delete.append(filename)
 
-for filename in files_to_delete:
-    filepath = "test/" + filename
-    os.remove(filepath)
-
-                
-
-    
-    
-    
-    
+style_dict, count_dict = makeFileNameAndStylePairsCsv()
+# remove_nan(style_dict, DIR_PATH)
+#
+label_dict = get_label_dict(style_dict, DIR_PATH)
+# make_label_txt_file(style_dict, label_dict, DIR_PATH)
