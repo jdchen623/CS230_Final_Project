84c71a2ca75750d02e54c09ee9a3f0489b825e49
jdchen623 <jchen623@stanford.edu>
2019-02-28 17:21:06 -0800

    training works without errors

--- PyTorch-ResNet50.py
+++ PyTorch-ResNet50.py
@@ -148,7 +148,7 @@ def train_model(model, criterion, optimizer, num_epochs=15):
 # In[ ]:
 
 
-model_trained = train_model(model, criterion, optimizer, num_epochs=1)
+model_trained = train_model(model, criterion, optimizer, num_epochs=15)
 
 
 # ### 5. Save and load the model
