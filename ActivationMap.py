8be8e2ceb4c2b25c1f74f5bcbd9c7fbb833e0821
jdchen623 <jchen623@stanford.edu>
2019-03-13 20:01:10 -0700

    more changes to activation map, testing on aws

--- ActivationMap.py
+++ ActivationMap.py
@@ -37,3 +37,35 @@ prediction_var = Variable((tensor.unsqueeze(0)).cuda(), requires_grad=True)
 model = models.resnet50(pretrained=False).to(device)
 model.cuda()
 model.eval()
+
+class SaveFeatures():
+    features=None
+    def __init__(self, m): self.hook = m.register_forward_hook(self.hook_fn)
+    def hook_fn(self, module, input, output): self.features = ((output.cpu()).data).numpy()
+    def remove(self): self.hook.remove()
+
+final_layer = model._modules.get('layer4')
+activated_features = SaveFeatures(final_layer)
+
+prediction = model(prediction_var)
+pred_probabilities = F.softmax(prediction).data.squeeze()
+activated_features.remove()
+
+topk(pred_probabilities,1)
+
+def getCAM(feature_conv, weight_fc, class_idx):
+    _, nc, h, w = feature_conv.shape
+    cam = weight_fc[class_idx].dot(feature_conv.reshape((nc, h*w)))
+    cam = cam.reshape(h, w)
+    cam = cam - np.min(cam)
+    cam_img = cam / np.max(cam)
+    return [cam_img]
+
+weight_softmax_params = list(model._modules.get('fc').parameters())
+weight_softmax = np.squeeze(weight_softmax_params[0].cpu().data.numpy())
+weight_softmax_params
+class_idx = topk(pred_probabilities,1)[1].int()
+overlay = getCAM(activated_features.features, weight_softmax, class_idx )
+imshow(overlay[0], alpha=0.5, cmap='jet')
+imshow(display_transform(image))
+imshow(skimage.transform.resize(overlay[0], tensor.shape[1:3]), alpha=0.5, cmap='jet');
